8302927110108d801934209622c55ac7e34bcb75
Matevž Špacapan <6719426+Pur3Bolt@users.noreply.github.com>
2020-12-16 11:51:05 +0100

    Export drawing function

--- main.js
+++ main.js
@@ -34,11 +34,28 @@ window.onload=function(){
 		}
 	});
 };
+
+//function for exporting the line data
+function exportNotes(){
+  var element = document.createElement('a');
+  var exporting="";
+  for (var i = 0; i < notes.length; i++) {
+    exporting+=notes[i].join(";")+"\n";
+  }
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(exporting));
+  element.setAttribute('download', "notes.txt");
+  element.style.display = 'none';
+  document.body.appendChild(element);
+  element.click();
+  document.body.removeChild(element);
+}
+
 //set color of line
 function colorSet(newcolor){
   color=newcolor;
   mode=true;
 }
+
 //draw a single line on the canvas
 function draw(color, x0, y0, x, y){
   ctx.beginPath();
@@ -48,6 +65,7 @@ function draw(color, x0, y0, x, y){
   ctx.stroke();
   ctx.closePath();
 }
+
 //clear canvas, draw all lines on the canvas stored in the array and the current line if we're in drawing mode
 function drawAll(){
   clearCanvas();
@@ -58,18 +76,22 @@ function drawAll(){
     draw(color, x0, y0, x, y);
   }
 }
+
 //clear the entire canvas
 function clearCanvas(){
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
+
 //change mode to removing lines
 function eraser(){
   mode=false;
 }
+
 //calculate distance between point A and B
 function distance2(a, b){
   return Math.sqrt(Math.pow(a[0]-b[0], 2)+Math.pow(a[1]-b[1], 2));
 }
+
 //return the sum of distances of points AB+BC (B being the mouse cursor)
 //and the distance between AC (line start & end)
 function distance3(a, b, c){
@@ -77,11 +99,13 @@ function distance3(a, b, c){
       ac=distance2([a[0], a[1]], [c[0], c[1]]);
   return [abc.toFixed(1), ac.toFixed(1)];
 }
+
 //return if we're on the left or right side of a line
 function sideOfNote(x, y, note){
   var d=(x-note[1])*(note[4]-note[2])-(y-note[2])*(note[3]-note[1]);
   return d<0;
 }
+
 //return speed of mouse used for note velocity
 function mouseVelocity(){
   time0 = Date.now();
@@ -91,17 +115,22 @@ function mouseVelocity(){
   time = time0;
   return Math.round(10*Math.sqrt(x_dist*x_dist+y_dist*y_dist)/interval);
 }
+
 //map values between two ranges [A-B]->[C-D]
 function map(x, in_min, in_max, out_min, out_max){
   return (x-in_min)*(out_max-out_min)/(in_max-in_min)+out_min;
 }
-//
+
+//function for deciding the action we're doing and the coordinates of the action
 function findxy(action, e){
+
+  //set starting point of line
   if(action=='down'){
-    //set starting point of line
     x0=e.clientX-canvas.offsetLeft;
     y0=e.clientY-canvas.offsetTop;
   }
+
+  //mouse button was released
   if(action=='up'){
     //save line to array to redraw always
     if(mode)
@@ -115,12 +144,16 @@ function findxy(action, e){
     if(mode)
       drawAll();
   }
+
+  //the mouse was moved to a different spot
   if(action=='move'){
     x=e.clientX-canvas.offsetLeft;
     y=e.clientY-canvas.offsetTop;
+
     //draw line from start point to current point if mouse was pressed down before
     if(x0!=-1 && y0!=-1 && mode)
       drawAll();
+
     //erase lines we cross if mouse was pressed down before
     else if(x0!=-1 && y0!=-1 && !mode){
       for(var i=0;i<notes.length;i++){
@@ -132,6 +165,7 @@ function findxy(action, e){
         }
       }
     }
+
     //play note if we're not holding the mouse button
     else{
       for(var i=0;i<notes.length;i++){
@@ -140,8 +174,10 @@ function findxy(action, e){
         if(notes[i][5]==null){
           notes[i][5]=d;
         }
+
         //if mousePos in note is opposite to current AND distances is returning true, then play note
         var distances=distance3([notes[i][1], notes[i][2]], [x, y], [notes[i][3], notes[i][4]]);
+
         if(notes[i][5]!=d && distances[0]==distances[1]){
           //calculate mouse velocity
           var velocity = Math.min(127, mouseVelocity());
@@ -171,6 +207,7 @@ function findxy(action, e){
           MIDI.noteOff(instrument, note, 1);
           console.log(note+" "+velocity);
         }
+
         //update side of note mouse is at
         notes[i][5]=d;
       }
